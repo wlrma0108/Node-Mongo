6ecae8efd0c7c19b15f7bff9d767133f6912dd77
wlrma0108 <dlwlrma0108@gmail.com>
2022-07-04 23:35:25 +0900

    commit

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
+const body = require('body-parser');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(8080, function() {
     console.log('listening on 8080')
-})
+});
+
+
+app.get('/write',function(request,response){
+    response.sendFile(__dirnema+'/write.html')
+});
+
+app.post('/add',function(request,response){
+    response.send('post')
+});
