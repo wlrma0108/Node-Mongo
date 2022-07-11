f3ae546d40f25701fd3205e35b8845a8fd2c06f3
wlrma0108 <dlwlrma0108@gmail.com>
2022-07-11 23:04:04 +0900

    commit

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const express = require('express');
 const app = express();
 const body = require('body-parser');
 const bodyParser = require('body-parser');
+const { Db } = require('mongodb');
 const MongoClient = require('mongodb').MongoClient;
 
 
 app.use(bodyParser.urlencoded({extended: true}));
-
-app.listen(8080, function() {
-    console.log('listening on 8080')
-});
 
 
 app.get('/write',function(request,response){
@@ -19,4 +16,13 @@ app.get('/write',function(request,response){
 
 app.post('/add',function(request,response){
     response.send('post')
+    Db.collection('post').insertOne({title: ' ', day : '  '}, function(){});
 });
+
+MongoClient.connect('', function(error, client){
+    if (error) return console.log(error);
+    app.listen('8080', function(){
+      console.log('listening on 8080')
+      
+    });
+  })
